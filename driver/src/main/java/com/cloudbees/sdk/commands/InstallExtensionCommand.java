58f7633fc2910d079ab6aa4f0dfa38813d1ff8ae
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-01-11 22:15:48 -0800

    added a command to install an extension

---
+++ driver/src/main/java/com/cloudbees/sdk/commands/InstallExtensionCommand.java
@@ -0,0 +1,57 @@
+package com.cloudbees.sdk.commands;
+
+import com.cloudbees.sdk.AbstractCommand;
+import com.cloudbees.sdk.ArtifactClassLoaderFactory;
+import com.cloudbees.sdk.CLICommand;
+import com.cloudbees.sdk.CommandDescription;
+import com.cloudbees.sdk.Experimental;
+import com.cloudbees.sdk.GAV;
+import com.cloudbees.sdk.InstalledExtensionList;
+import com.google.inject.Provider;
+import org.kohsuke.args4j.Argument;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@Experimental
+@CLICommand("install-extension")
+@CommandDescription("Installs a CLI extension")
+public class InstallExtensionCommand extends AbstractCommand {
+    @Inject
+    InstalledExtensionList list;
+
+    @Argument(metaVar="GAV",required=true,usage="groupId:artifact[:version] of the extension to install")
+    String gav;
+
+    @Inject
+    Provider<ArtifactClassLoaderFactory> artifactClassLoaderFactoryProvider;
+    
+    @Override
+    public int main() throws Exception {
+        GAV gav = parseGav();
+
+        // resolve the artifact to make sure it's valid
+        try {
+            artifactClassLoaderFactoryProvider.get().add(gav);
+        } catch (Exception e) {
+            throw (IOException)new IOException("Failed to resolve "+gav).initCause(e);
+        }
+
+        // record it
+        list.put(gav.groupId+'.'+gav.artifactId,gav);
+
+        return 0;
+    }
+
+    private GAV parseGav() {
+        String[] tokens = gav.split(":");
+        if (tokens.length==3)
+            return new GAV(gav);
+        else
+            return new GAV(tokens[0],tokens[1],"LATEST");
+    }
+}
+
